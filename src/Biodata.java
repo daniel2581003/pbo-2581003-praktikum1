aac489c98d743d127470680c13d5c1a5d13db462
Daniel Lim <2581003@unai.edu>
2026-09-04 15:35:18 +0700

    Nama dan NIM

---
+++ src/Biodata.java
@@ -0,0 +1,8 @@
+public class Biodata {
+    public static void main(String[] args) {
+        String nama = "Daniel Lim";
+        String nim  = "2581003";
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM  : " + nim);
+    }
+}

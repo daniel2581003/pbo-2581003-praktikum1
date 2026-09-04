1073ac1f17ccd8eba05369c41e3d894ffc0f10f6
Daniel Lim <2581003@unai.edu>
2026-09-04 15:39:24 +0700

    Program Studi dan Asal Daerah

--- src/Biodata.java
+++ src/Biodata.java
@@ -2,7 +2,12 @@ public class Biodata {
     public static void main(String[] args) {
         String nama = "Daniel Lim";
         String nim  = "2581003";
+        String prodi = "Teknik Informatika";
+        String asal = "Jakarta";
         System.out.println("Nama : " + nama);
         System.out.println("NIM  : " + nim);
+        System.out.println("Program Studi : " + prodi);
+        System.out.println("Asal Daerah : " + asal);
+
     }
 }

1bc735d2093b2c5b959f4660e7d1bfae4f5e7d03
Daniel Lim <2581003@unai.edu>
2026-09-04 15:43:32 +0700

    Alasan memilih Prodi dan Target Semester

--- src/Biodata.java
+++ src/Biodata.java
@@ -4,10 +4,14 @@ public class Biodata {
         String nim  = "2581003";
         String prodi = "Teknik Informatika";
         String asal = "Jakarta";
+        String alasan = "Alasan memilih Prodi karena Saya menyukai Komputer sejak saya kecil";
+        String target = "Target saya semester ini adalah membuat project sebanyak banyak nya agar dapat memenuhi Portofolio saya";
         System.out.println("Nama : " + nama);
         System.out.println("NIM  : " + nim);
         System.out.println("Program Studi : " + prodi);
         System.out.println("Asal Daerah : " + asal);
+        System.out.println("Asal Daerah : " + alasan);
+        System.out.println("Asal Daerah : " + target);
 
     }
 }
